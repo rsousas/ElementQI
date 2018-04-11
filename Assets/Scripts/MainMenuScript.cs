f545e1714bb55b6b112126e63b6ac3c728896157
rsousas <renatosousa_96@hotmail.com>
2018-04-11 09:44:17 -0300

    Primeiro Commit

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	public Texture fundoTexture;
+	public GUISkin MainMenuSkin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI (){
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fundoTexture);
+		GUI.skin = MainMenuSkin;
+
+		if (GUI.Button (new Rect (Screen.width / 2 - buttonWidth / 2, Screen.height / 2 - buttonHeight / 2, buttonWidth, buttonHeight), "JOGAR")) {
+			SceneManager.LoadScene(1);
+		}
+
+		if (Input.anyKeyDown)
+			SceneManager.LoadScene(1);
+
+	}
+}
