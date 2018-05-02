83d80429b7785e79982c969ee624645d6d766f76
Thiago Melo <thiagomelosg@gmail.com>
2018-05-02 19:49:47 -0300

    Criação de x e de Card

---
+++ Assets/Script/ElementClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementClass : MonoBehaviour {
+
+    public string name;
+    public int minLeftLength;
+    public int minRightLength;
+    public int minTopLength;
+    public int minBottomLength;
+    public int[] initialPosicion;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Script/GeraGrids.cs
+++ Assets/Script/GeraGrids.cs
@@ -1,51 +1,128 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GeraGrids : MonoBehaviour {
+public class GeraGrids : MonoBehaviour
+{
 
-	private int posX, posY;
+    private int posX, posY;
 
-	private float MapaJogadorX = -8;
-	private float MapaJogadorY = -3;
+    private float MapaJogadorX = -8;
+    private float MapaJogadorY = -3;
 
-	private float MapaAdversarioX = 1;
-	private float MapaAdversarioY = -3;
+    private float MapaAdversarioX = 1;
+    private float MapaAdversarioY = -3;
 
-	private float MapaX;
-	private float MapaY;
+    private float MapaX;
+    private float MapaY;
 
-	public GameObject ObjetoFundo;
-	public GameObject ObjetoGrid;
+    public GameObject ObjetoGrid;
+    public GameObject ObjetoErro;
+    public GameObject ObjetoCard;
+    public ushort[,] Map = new ushort[30, 15];
 
-	// Use this for initialization
-	void Start () {
-		for (posX = -10; posX <=10 ; posX++)
-			for (posY =-5; posY <=5; posY++)
-				Instantiate(ObjetoFundo, new Vector3(posX, posY, 0), Quaternion.identity);
+    // Use this for initialization
+    void Start()
+    {
+        gerarGrid();
+        geraFundoErro();
+        geraCard();
+    }
 
-		MapaX = MapaJogadorX;
-		MapaY = MapaJogadorY;
-		for (posX = 1; posX <=33 ; posX++)
-		{	for (posY =1; posY <=15; posY++)
-			{	
-				Instantiate(ObjetoGrid, new Vector3(MapaX, MapaY, -1), Quaternion.identity);
-				MapaY = MapaY + 0.5f;
-			}
-			MapaY = MapaJogadorY;
-			MapaX = MapaX + 0.5f;
-		}
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left mouse clicked");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-	/*	
-	    MapaX = MapaAdversarioX;
-		MapaY = MapaAdversarioY;
-		for (posX = 1; posX <=15 ; posX++)
-		{	for (posY =1; posY <=15; posY++)
-			{	
-				Instantiate(ObjetoGrid, new Vector3(MapaX, MapaY, -1), Quaternion.identity);
-				MapaY = MapaY + 0.5f;
-			}
-			MapaY = MapaAdversarioY;
-			MapaX = MapaX + 0.5f;
-		}*/
-	}
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                Debug.Log("Active piece: " + hit.transform.gameObject.name.ToString());
+
+                if (hit.transform.tag == "error")
+                    Debug.Log("Errou");
+
+                if (hit.transform.tag == "card")
+                {
+                    Destroy(hit.transform.gameObject);
+                    Debug.Log("Card");
+                }
+            }
+        }*/
+    }
+
+    void gerarGrid()
+    {
+        for (posX = 0; posX < 30; posX++)
+        {
+            for (posY = 0; posY < 15; posY++)
+            {
+                Map[posX, posY] = 1;
+            }
+        }
+
+        for (posX = 0; posX < 13; posX++)
+            Map[posX, 0] = 0;
+
+        for (posX = 0; posX < 12; posX++)
+            Map[posX, 1] = 0;
+
+        for (posX = 0; posX < 11; posX++)
+            Map[posX, 2] = 0;
+
+        for (posX = 0; posX < 10; posX++)
+            Map[posX, 3] = 0;
+
+        Map[27, 0] = Map[28, 0] = Map[29, 0] = Map[28, 1] = Map[29, 1] = Map[29, 2] = 0;
+        Map[0, 4] = Map[1, 4] = Map[2, 4] = Map[0, 5] = Map[1, 5] = Map[0, 6] = 0;
+        Map[27, 10] = Map[28, 10] = Map[28, 9] = Map[29, 10] = Map[29, 9] = Map[29, 8] = 0;
+        Map[0, 12] = Map[0, 13] = Map[0, 14] = Map[1, 13] = Map[1, 14] = Map[2, 14] = 0;
+
+        for (posX = 20; posX < 30; posX++)
+            Map[posX, 11] = 0;
+
+        for (posX = 19; posX < 30; posX++)
+            Map[posX, 12] = 0;
+
+        for (posX = 18; posX < 30; posX++)
+            Map[posX, 13] = 0;
+
+        for (posX = 17; posX < 30; posX++)
+            Map[posX, 14] = 0;
+
+        for (posX = 0; posX < 30; posX++)
+        {
+            for (posY = 0; posY < 15; posY++)
+            {
+                if (Map[posX, posY] == 1)
+                    Instantiate(ObjetoGrid, new Vector3(posX * 0.6f - 8.75f, posY * 0.6f - 3.25f, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    void geraFundoErro()
+    {
+        for (posX = 0; posX < 30; posX++)
+        {
+            for (posY = 0; posY < 15; posY++)
+            {
+                if (Map[posX, posY] == 1)
+                    Instantiate(ObjetoErro, new Vector3(posX * 0.6f - 8.75f, posY * 0.6f - 3.25f, -0.01f), Quaternion.identity);
+            }
+        }
+    }
+
+    void geraCard()
+    {
+        for (posX = 0; posX < 30; posX++)
+        {
+            for (posY = 0; posY < 15; posY++)
+            {
+                if (Map[posX, posY] == 1)
+                    Instantiate(ObjetoCard, new Vector3(posX * 0.6f - 8.75f, posY * 0.6f - 3.25f, -0.02f), Quaternion.identity);
+            }
+        }
+    }
 }
