d44f02edc0daedb9c0398ab66c6077fb17e5a1e9
Thiago Melo <thiagomelosg@gmail.com>
2018-05-20 19:08:23 -0300

    Alterações visuais na vez do player e nos prefabs dos elementos.

--- Assets/Script/Level1Script.cs
+++ Assets/Script/Level1Script.cs
@@ -6,11 +6,13 @@ public class Level1Script : MonoBehaviour {
 
 	public static int scoresPlayer1 = 0;
 	public static int scoresPlayer2 = 0;
+    public GUIStyle styleTurn, styleWait;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        styleTurn.normal.textColor = Color.green;
+        styleWait.normal.textColor = Color.gray;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,7 +21,13 @@ public class Level1Script : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Label (new Rect (10, 10, 105, 20), "Player 1:" + Level1Script.scoresPlayer1.ToString ());
-		GUI.Label (new Rect (10, 30, 105, 20), "Player 2:" + Level1Script.scoresPlayer2.ToString ());
+        if (GeraGrids.player) {
+            GUI.Label(new Rect(20, 30, 105, 20), "Player 1:" + Level1Script.scoresPlayer1.ToString(), styleTurn);
+            GUI.Label(new Rect(20, 60, 105, 20), "Player 2:" + Level1Script.scoresPlayer2.ToString(), styleWait);
+        }
+        else {
+            GUI.Label(new Rect(20, 30, 105, 20), "Player 1:" + Level1Script.scoresPlayer1.ToString(), styleWait);
+            GUI.Label(new Rect(20, 60, 105, 20), "Player 2:" + Level1Script.scoresPlayer2.ToString(), styleTurn);
+        }
 	}
 }
